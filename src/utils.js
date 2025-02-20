77fa3c5f47c10f44ba200f3d6aeeb50fc5fcd95d
Ewald Horn <ewaldhorn@gmail.com>
2025-02-20 10:24:46 +0200

    Add some JS utils

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,19 @@
 // ----------------------------------------------------------------------------
+/**
+ * Returns a random boolean value with 50% probability of being true
+ * @returns {boolean} - Returns true or false with equal probability
+ */
 export const isRandomTrue = () => {
   return Math.random() > 0.5;
+};
+
+// ----------------------------------------------------------------------------
+/**
+ * Returns a random integer between two numbers, inclusive
+ * @param {number} start - The minimum value (inclusive)
+ * @param {number} end - The maximum value (inclusive)
+ * @returns {number} - A random integer between start and end
+ */
+export const randomBetween = (start, end) => {
+  return start + Math.floor(Math.random() * (end - start + 1));
 };
